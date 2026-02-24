91601e507fe84ea0d9d2f1902245fe71e3b279ef
subhgupta007 <subhgupta@deloitte.com>
2026-02-24 11:48:35 +0530

    added bucket name and table in name in main.tf

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -1,6 +1,6 @@
 terraform { 
   backend "s3" {
-    bucket = "tl-bucket"
+    bucket = "tl-digit-bucket"
     key    = "terraform-setup/terraform.tfstate"
     region = "ap-south-1"
     # The below line is optional depending on whether you are using DynamoDB for state locking and consistency
