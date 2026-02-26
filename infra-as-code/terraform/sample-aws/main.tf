27ffc26b61f688cc8692256a19d0b3d978a5fdc7
subhgupta007 <subhgupta@deloitte.com>
2026-02-26 13:37:41 +0530

    change in main.tf

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "terraform-setup/terraform.tfstate"
     region = "ap-south-1"
     # The below line is optional depending on whether you are using DynamoDB for state locking and consistency
-    dynamodb_table = "tl-state-s3-bucket-lock-table"
+    dynamodb_table = "tl-state-s3-bucket"
     # The below line is optional if your S3 bucket is encrypted
     encrypt = true
   }
